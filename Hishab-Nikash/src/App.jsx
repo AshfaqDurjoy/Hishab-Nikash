06fddb5353da315651041340292ea1754a263f0d
TanzilaTabassum <131589302+TanzilaTabassum@users.noreply.github.com>
2025-09-07 01:20:29 +0600

    App.jsx

--- Hishab-Nikash/src/App.jsx
+++ Hishab-Nikash/src/App.jsx
@@ -1,111 +1,38 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
-import AppSidebar from "./Components/AppSidebar";
-import Topbar from "./Components/Topbar";
-import Dashboard from "./Pages/Dashboard"; 
-import Budget from "./Pages/Budget";
-import TransactionPage from "./Pages/TransactionPage";
-import Report from "./Pages/Report";
-import Profile from "./Pages/Profile";
-import Settings from "./Pages/Settings";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AppSidebar from "./Components/AppSidebar"; 
+import Topbar from "./Components/Topbar"; 
+import Dashboard from "./Pages/Dashboard";
+import Budget from "./Pages/Budget"; 
+import TransactionPage from "./Pages/TransactionPage"; 
+import Report from "./Pages/Report"; 
+import Profile from "./Pages/Profile"; 
+import Settings from "./Pages/Settings"; 
+import Login from "./Pages/Login"; 
+import Signup from "./Pages/Signup"; 
 import Startup from "./Components/Startup";
 
-const App = () => {
-  const [currentPage, setCurrentPage] = useState("dashboard"); 
-
-  return (
+const App = () => { 
+  const [currentPage, setCurrentPage] = useState("dashboard");
+  return(
     <Router>
-      
-
-    <Routes>
+<Routes>
   
   <Route path="/" element={<Startup />} />
-  <Route path="/login" element={<Login />} />
+
+  
+  <Route path="/dashboard" element={<Dashboard />} />
+  <Route path="/budget" element={<Budget />} />
+  <Route path="/transactionPage" element={<TransactionPage />} />
+  <Route path="/report" element={<Report />} />
+  <Route path="/profile" element={<Profile />} />
+  <Route path="/settings" element={<Settings />} />
+
   <Route path="/signup" element={<Signup />} />
-
-  <Route
-    path="/dashboard"
-    element={
-      <div className="flex">
-        <AppSidebar />
-        <div className="ml-64 bg-gray-100 w-full">
-          <Topbar currentPage={currentPage} />
-          <Dashboard />
-        </div>
-      </div>
-    }
-  />
-
-  <Route
-    path="/budget"
-    element={
-      <div className="flex">
-        <AppSidebar />
-        <div className="ml-64 bg-gray-100 w-full">
-          <Topbar currentPage={currentPage} />
-          <Budget />
-        </div>
-      </div>
-    }
-  />
-
-  <Route
-    path="/transactionPage"
-    element={
-      <div className="flex">
-        <AppSidebar />
-        <div className="ml-64 bg-gray-100 w-full">
-          <Topbar currentPage={currentPage} />
-          <TransactionPage />
-        </div>
-      </div>
-    }
-  />
-
-  <Route
-    path="/report"
-    element={
-      <div className="flex">
-        <AppSidebar />
-        <div className="ml-64 bg-gray-100 w-full">
-          <Topbar currentPage={currentPage} />
-          <Report />
-        </div>
-      </div>
-    }
-  />
-
-  <Route
-    path="/profile"
-    element={
-      <div className="flex">
-        <AppSidebar />
-        <div className="ml-64 bg-gray-100 w-full">
-          <Topbar currentPage={currentPage} />
-          <Profile />
-        </div>
-      </div>
-    }
-  />
-
-  <Route
-    path="/settings"
-    element={
-      <div className="flex">
-        <AppSidebar />
-        <div className="ml-64 bg-gray-100 w-full">
-          <Topbar currentPage={currentPage} />
-          <Settings />
-        </div>
-      </div>
-    }
-  />
+  <Route path="/login" element={<Login />} />
 </Routes>
 
-    </Router>
+      </Router>
   );
 };
-
 export default App;
